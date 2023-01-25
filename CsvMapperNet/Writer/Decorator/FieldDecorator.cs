c478f65bbc2edb84fcf9af6ca8801a2a827cd7c3
u-u2 <u-u2@outlook.jp>
2023-01-25 16:14:36 +0900

    add FieldDecorator to use when writing fields

---
+++ CsvMapperNet/Writer/Decorator/FieldDecorator.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace CsvMapperNet.Writer.Decorator {
+	internal class FieldDecorator {
+
+		private readonly char _delimiter;
+
+		public FieldDecorator(char delimiter) {
+			_delimiter = delimiter;
+		}
+
+		public string Decorate(string field) {
+			if (field.Contains(_delimiter)) {
+				return $"\"{field}\"";
+			}
+			return field;
+		}
+
+	}
+}
